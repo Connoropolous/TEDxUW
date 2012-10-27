85e26362a3288ccd6c94d750dd277b9bb07ca37b
Lucas Zeer <lucas.zeer@gmail.com>
2012-10-27 17:19:18 -0400

    asdfgh

--- formtools/global/config.php
+++ formtools/global/config.php
@@ -1,13 +1,13 @@
+
 <?php
 
 // main program paths - no trailing slashes!
-$g_root_url = "http://tedx.localhost.com/formtools";
-$g_root_dir = "C:\\Users\\Connor\\Documents\\CodeCraft\\TEDxUW\\formtools";
-
+$g_root_url = "http://localhost/~lucaszw/TEDxUW/formtools";
+$g_root_dir = "/Users/lucaszw/Sites/TEDxUW/formtools";
 // database settings
 $g_db_hostname = "localhost";
 $g_db_name = "tedxuw";
 $g_db_username = "tedxuw";
-$g_db_password = "1sdf\$fas#A";
+$g_db_password = "1234";
 $g_table_prefix = "ft_";
 ?>
